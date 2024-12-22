26957ba40bde68dc7b319eae995dfc2a0211b713
tasheT-T <tinasheriley@gmail.com>
2024-12-22 10:27:34 +0200

    Form validation p2

--- script.js
+++ script.js
@@ -4,19 +4,25 @@ document.getElementById("submit").addEventListener("click", function (event) {
     const inputs = document.querySelectorAll(".form input");
     let SubmitEvent = document.getElementById("submit");
     let isValid = true;
+    let errorMessage = '';
 
     // Loop through inputs and check if they are empty
     inputs.forEach(input => {
         if (input.value.trim() === "") {
             isValid = false;
             input.style.border = "2px solid red"; 
+            errorMessage = 'Please fill out all fields before submitting.';
+        } else if (isNaN(input.value)) {
+            isValid = false;
+            input.style.border = "2px solid red";
+            errorMessage = 'Please enter valid numbers.';
         } else {
             input.style.border = "";
         }
     });
 
     if (!isValid) {
-        alert("Please fill out all fields before submitting.");
+        alert(errorMessage);
     } else {
         SubmitEvent.style.backgroundColor = "green";
     }
